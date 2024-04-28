829861e20776943a125555636260b39a8cbe640a
Leopls <leonardobento233@gmail.com>
2024-04-27 21:42:50 -0300

    FEAT: Cadastro oficina + cadastro gerente

--- institucional-.motion/src/components/formulario/FormularioConcluido.jsx
+++ institucional-.motion/src/components/formulario/FormularioConcluido.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from "react";
 import styles from "./Formulario.module.css";
 import images from "../../utils/imagesExports";
@@ -7,78 +6,15 @@ import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import api from "../../api";
 
-const FormularioConcluido = ({ data }) => {
-    const [redirect, setRedirect] = useState(false);
-    const navigate = useNavigate();
-    const [objetoAdicionadoEmpresa, setObjetoAdicionadoEmpresa] = useState({});
-    const [objetoAdicionadoGerente, setObjetoAdicionadoGerente] = useState({});
-    const [idOficina, setIdOficina] = useState(null);
-
-    function cadastrar() {
-        setObjetoAdicionadoEmpresa(
-            {
-                nome: data.nomeEmpresa,
-                cnpj: data.cnpj,
-                cep: data.cep,
-                numero: data.numero,
-                complemento: data.complemento,
-                hasBuscar: data.hasBuscar
-            }
-        )
-        api.post(`/oficinas`, objetoAdicionadoEmpresa).then((response) => {
-            setIdOficina(response.id);
-            toast.success("Oficina cadastrada!")
-        }).catch((error) => {
-            console.error("Erro ao salvar os dados de oficina:", error);
-            toast.error("Ocorreu um erro ao salvar os dados de oficina, por favor, tente novamente.");
-            return false;
-        });
-
-        setObjetoAdicionadoGerente(
-            {
-                email: data.email,
-                nome: data.nome,
-                sobrenome: data.sobrenome,
-                fkOficina: idOficina
-            }
-        )
-        
-        api.post(`/gerentes`, objetoAdicionadoGerente).then(() =>{
-            toast.success("Gerente cadastrado!")
-            return true;
-        }).catch((error) => {
-            console.error("Erro ao salvar os dados de oficina:", error);
-            toast.error("Ocorreu um erro ao salvar os dados do gerente, por favor, tente novamente.");
-            return false;
-        })
-
-        
-    }
-
-    // useEffect(() => {
-    //     if(cadastrar()){
-    //         const timer = setTimeout(() => {
-    //             setRedirect(true);
-    //         }, 5000);
-    //         return () => clearTimeout(timer);
-    //     } else{
-    //         return false;
-    //     }
-    // }, []);
-
-    useEffect(() => {
-        cadastrar();
-        if (redirect) {
-            navigate("/pitstop");
-        }
-    }, [redirect]);
-
-    return (
-        <section id={styles["section-info"]} className={styles["concluido"]}>
-            <img src={images.checkConcluido} alt="Cadastro realizado"></img>
-            <h1>Cadastro Realizado!</h1>
-        </section>
-    );
+const FormularioConcluido = () => {
+  const navigate = useNavigate();
+  
+  return (
+    <section id={styles["section-info"]} className={styles["concluido"]}>
+      <img src={images.checkConcluido} alt="Cadastro realizado"></img>
+      <h1>Cadastro Realizado!</h1>
+    </section>
+  );
 };
 
 export default FormularioConcluido;
--- institucional-.motion/src/components/formulario/FormularioPlanos.jsx
+++ institucional-.motion/src/components/formulario/FormularioPlanos.jsx
@@ -1,119 +1,144 @@
-
 import React, { useState } from "react";
 import styles from "./Formulario.module.css";
-import images from "./../../utils/imagesExports"
+import images from "./../../utils/imagesExports";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
+import api from "../../api";
+import Loader from "../loader/Loader";
 
 const FormularioPlanos = ({ data }) => {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false)
 
-    const navigate = useNavigate();
-
-    function mudarPagina(buscar) {
-        Swal.fire({
-            title: "Tem certeza?",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Quero este plano!"
-        }).then((result) => {
-            if (result.isConfirmed) {
-                Swal.fire({
-                    title: "Confirmado!",
-                    text: buscar ? "Você escolheu o plano Buscar!" : "Você escolheu o plano Pitstop!",
-                    icon: "success"
-                });
-                navigate("/cadastro", {
-                    state: {
-                        form: 3,
-                        qtdConcluido: 4,
-                        data: {
-                            email: data.email,
-                            nome: data.nome,
-                            sobrenome: data.sobrenome,
-                            telefone: data.telefone,
-                            nomeEmpresa: data.nomeEmpresa,
-                            cnpj: data.cnpj,
-                            cep: data.cep,
-                            logradouro: data.logradouro,
-                            numero: data.numero,
-                            comp: data.comp,
-                            bairro: data.bairro,
-                            cidade: data.cidade,
-                            estado: data.estado,
-                            buscar
-                        }
-                    }
-                });
-                console.log(data);
+  async function mudarPagina() {
+    let isConfirmed = await Swal.fire({
+      title: "Tem certeza?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Quero este plano!",
+    }).then((result) => {
+      return result.isConfirmed;
+    });
+    if (isConfirmed) {
+      let cadastrou = await cadastrar();
+      ;
+      if(cadastrou){
+          navigate("/cadastro",{
+            state:{
+              form:3,
+              qtdConcluido:4
             }
-        });
+          })
+      } 
+      
     }
+  }
 
-    return (
-        <section id={styles["section-info"]}>
-            <h1>Escolher Plano</h1>
-            <div className={styles["container-planos"]}>
-                <div className={styles["plano"]}>
-                    <div className={styles["box"]}>
-                        <div className={styles["preco"]}>
-                            <h1 className={styles["titulo-preco"]}>R$300</h1>
-                            <p>/por mês</p>
-                        </div>
-                        <div className={styles["linha"]}>
-                            <img src={images.checkConcluido} alt="Imagem de Confirmação" />
-                            <p>Módulos Completos</p>
-                        </div>
-                        <div className={styles["linha"]}>
-                            <img src={images.checkConcluido} alt="Imagem de Confirmação" />
-                            <p>Relatórios e Dashboards</p>
-                        </div>
-                        <div className={styles["linha"]}>
-                            <img src={images.botaoX} alt="Imagem de Não Confirmado" />
-                            <p>Impulsionamento com Buscar!</p>
-                        </div>
-                        <div className={styles["linha"]}>
-                            <img src={images.botaoX} alt="Imagem de Não Confirmado" />
-                            <p>Integração com o Whatsapp</p>
-                        </div>
-                    </div>
-                    <div className={styles["botao"]}>
-                        <a onClick={() => mudarPagina(false)}>Escolher</a>
-                    </div>
-                </div>
-                <div className={styles["plano"]}>
-                    <div className={styles["box"]}>
-                        <div className={styles["preco"]}>
-                            <h1>R$350</h1>
-                            <p>/por mês</p>
-                        </div>
-                        <div className={styles["linha"]}>
-                            <img src={images.checkConcluido} alt="Imagem de Confirmação" />
-                            <p>Módulos Completos</p>
-                        </div>
-                        <div className={styles["linha"]}>
-                            <img src={images.checkConcluido} alt="Imagem de Confirmação" />
-                            <p>Relatórios e Dashboards</p>
-                        </div>
-                        <div className={styles["linha"]}>
-                            <img src={images.checkConcluido} alt="Imagem de Confirmação" />
-                            <p>Impulsionamento com Buscar!</p>
-                        </div>
-                        <div className={styles["linha"]}>
-                            <img src={images.checkConcluido} alt="Imagem de Confirmação" />
-                            <p>Integração com o Whatsapp</p>
-                        </div>
-                    </div>
-                    <div className={styles["botao"]}>
-                        <a style={{ cursor: "pointer" }} onClick={() => mudarPagina(true)}>Escolher</a>
-                    </div>
-                </div>
+
+  async function cadastrar() {
+    setIsLoading(true);
+    const oficina = {
+      nome: data.nomeEmpresa,
+      cnpj: data.cnpj,
+      cep: data.cep,
+      numero: data.numero,
+      complemento: data.complemento,
+      hasBuscar: data.hasBuscar,
+    };
+    try{
+        const resOficina = await api.post(`/oficinas`, oficina).then((response) => {
+          console.log(response)  
+          return {
+                email: data.email,
+                nome: data.nome,
+                sobrenome: data.sobrenome,
+                fkOficina: response.data.idOficina,
+            };
+        });
+        console.log(resOficina);
+        const resGerente = await api.post(`/gerentes`, resOficina).then((response) => {
+          console.log("Res Gerente: " + response);
+        });
+    }catch(error){
+        console.log(error.response)
+        console.error("Erro ao cadastrar-se:", error);
+        toast.error("Ocorreu um erro no cadastro por favor, tente novamente.");
+        setIsLoading(false);
+        return false;
+    }
+    setIsLoading(false);
+    return true;
+  }
+
+  return (
+    <>
+    <Loader show={isLoading} />
+    <section id={styles["section-info"]}>
+      <h1>Escolher Plano</h1>
+      <div className={styles["container-planos"]}>
+        <div className={styles["plano"]}>
+          <div className={styles["box"]}>
+            <div className={styles["preco"]}>
+              <h1 className={styles["titulo-preco"]}>R$300</h1>
+              <p>/por mês</p>
             </div>
-        </section>
-    );
-
-}
+            <div className={styles["linha"]}>
+              <img src={images.checkConcluido} alt="Imagem de Confirmação" />
+              <p>Módulos Completos</p>
+            </div>
+            <div className={styles["linha"]}>
+              <img src={images.checkConcluido} alt="Imagem de Confirmação" />
+              <p>Relatórios e Dashboards</p>
+            </div>
+            <div className={styles["linha"]}>
+              <img src={images.botaoX} alt="Imagem de Não Confirmado" />
+              <p>Impulsionamento com Buscar!</p>
+            </div>
+            <div className={styles["linha"]}>
+              <img src={images.botaoX} alt="Imagem de Não Confirmado" />
+              <p>Integração com o Whatsapp</p>
+            </div>
+          </div>
+          <div className={styles["botao"]}>
+            <a onClick={() => mudarPagina(false)}>Escolher</a>
+          </div>
+        </div>
+        <div className={styles["plano"]}>
+          <div className={styles["box"]}>
+            <div className={styles["preco"]}>
+              <h1>R$350</h1>
+              <p>/por mês</p>
+            </div>
+            <div className={styles["linha"]}>
+              <img src={images.checkConcluido} alt="Imagem de Confirmação" />
+              <p>Módulos Completos</p>
+            </div>
+            <div className={styles["linha"]}>
+              <img src={images.checkConcluido} alt="Imagem de Confirmação" />
+              <p>Relatórios e Dashboards</p>
+            </div>
+            <div className={styles["linha"]}>
+              <img src={images.checkConcluido} alt="Imagem de Confirmação" />
+              <p>Impulsionamento com Buscar!</p>
+            </div>
+            <div className={styles["linha"]}>
+              <img src={images.checkConcluido} alt="Imagem de Confirmação" />
+              <p>Integração com o Whatsapp</p>
+            </div>
+          </div>
+          <div className={styles["botao"]}>
+            <a style={{ cursor: "pointer" }} onClick={() => mudarPagina(true)}>
+              Escolher
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+    </>
+  );
+};
 
 export default FormularioPlanos;
